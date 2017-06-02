9d21b12ce7c301aac0c2be14b6b310fe301d816d
Federico Marino Dragoset <fedemarino37@gmail.com>
2017-06-02 18:27:24 -0300

    Integrando interfaz del profesor

--- MessengerClient/src/Client/MessengerClient.java
+++ MessengerClient/src/Client/MessengerClient.java
@@ -4,6 +4,8 @@ import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
@@ -13,14 +15,21 @@ import javax.swing.JScrollPane;
 import javax.swing.JButton;
 import java.awt.Component;
 import javax.swing.Box;
+import javax.swing.DefaultListModel;
 import javax.swing.JTextField;
 import javax.swing.ScrollPaneConstants;
+import javax.swing.UIManager;
+
 import java.awt.GridBagConstraints;
 import java.awt.Insets;
 import java.awt.TextField;
 
 import javax.swing.JSplitPane;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.io.IOException;
 import java.net.Socket;
 import java.nio.channels.SocketChannel;
@@ -29,6 +38,7 @@ import java.awt.event.ActionEvent;
 import javax.swing.JMenuBar;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JTextPane;
 import javax.swing.JTextArea;
 
@@ -37,10 +47,21 @@ public class MessengerClient extends Thread {
 	private static ArrayList<Cliente> clientesConectados = new ArrayList<>();
 	Socket mySocket;
 	String mensaje;
+	private Boolean conectado = false;
+	
+	private JPanel contentPane;
+	private JList<String> listUsuarios;
+	private JLabel lblUsuarios;
+	private JFrame VC;
 
-	public static void main(String[] args) {
+	/*public static void main(String[] args) {
 		MessengerClient MC = new MessengerClient();
 		MC.crearChat();
+	}*/
+	
+	public static void main(String[] args) {
+		MessengerClient MC = new MessengerClient();
+		MC.crearVentanaCliente();
 	}
 
 	public void crearChat() {
@@ -60,6 +81,7 @@ public class MessengerClient extends Thread {
 			public void actionPerformed(ActionEvent e) {
 				try {
 					mySocket = new Socket("127.0.0.1", 9999);
+					conectado = true;
 				} catch (IOException e1) {
 					// TODO Auto-generated catch block
 					e1.printStackTrace();
@@ -71,11 +93,14 @@ public class MessengerClient extends Thread {
 		JMenuItem mntmDesconectar = new JMenuItem("Desconectar");
 		mntmDesconectar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				try {
-					mySocket.close();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+				if(conectado) {
+					try {
+						mySocket.close();
+						conectado  = false;
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
 				}
 			}
 		});
@@ -156,9 +181,168 @@ public class MessengerClient extends Thread {
 		frame.setVisible(true);
 	}
 	
-	
-	public MessengerClient() {
-				
-	}
 
+	public void crearVentanaCliente() {
+		VC = new JFrame();
+		VC.addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent arg0) {
+				abrirVentanaConfirmaSalir();
+			}
+		});
+		
+		try
+		{
+		   UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		}
+		catch (Exception e)
+		{
+		   e.printStackTrace();
+		}
+		
+		VC.setTitle("Chat");
+		VC.setResizable(false);
+		VC.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+		VC.setBounds(100, 100, 379, 526);
+		
+		JMenuBar menuBar = new JMenuBar();
+		VC.setJMenuBar(menuBar);
+		
+		JMenu mnArchivo = new JMenu("Archivo");
+		menuBar.add(mnArchivo);
+		
+		JMenuItem mntmConectar = new JMenuItem("Conectar");
+		mntmConectar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {
+					mySocket = new Socket("127.0.0.1", 9999);
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+		});
+		mnArchivo.add(mntmConectar);
+		
+		JMenuItem mntmSalir = new JMenuItem("Salir");
+		mntmSalir.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				abrirVentanaConfirmaSalir();
+			}
+		});
+		
+		JMenuItem mntmDesconectar_1 = new JMenuItem("Desconectar");
+		mntmDesconectar_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {
+					mySocket.close();
+				} catch (IOException f) {
+					// TODO Auto-generated catch block
+					f.printStackTrace();
+				}
+			}
+		});
+		mnArchivo.add(mntmDesconectar_1);
+		mnArchivo.add(mntmSalir);
+		
+		JMenu mnChat = new JMenu("Chat");
+		menuBar.add(mnChat);
+		
+		JMenuItem mntmSalaDeChat = new JMenuItem("Sala de Chat");
+		mntmSalaDeChat.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				new VentanaChat("", "Sala");
+			}
+		});
+		mnChat.add(mntmSalaDeChat);
+		
+		JMenuItem mntmSesionPrivada = new JMenuItem("Sesión privada");
+		mntmSesionPrivada.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				seleccionarElementoLista();
+			}
+		});
+		mnChat.add(mntmSesionPrivada);
+		
+		JMenu mnAyuda = new JMenu("Ayuda");
+		menuBar.add(mnAyuda);
+		
+		JMenuItem mntmConfigIpPuerto = new JMenuItem("Configurar IP-Puerto");
+		mntmConfigIpPuerto.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				abrirVentanaConfiguracion();
+			}
+		});
+		mnAyuda.add(mntmConfigIpPuerto);
+		
+		JMenuItem mntmAcerca = new JMenuItem("Acerca");
+		mnAyuda.add(mntmAcerca);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		VC.setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		scrollPane.setBounds(0, 0, 373, 462);
+		contentPane.add(scrollPane);
+		
+		listUsuarios = new JList<String>();
+		listUsuarios.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				seleccionaDobleClickChat(arg0);
+			}
+		});
+		scrollPane.setViewportView(listUsuarios);
+
+		lblUsuarios = new JLabel("Cantidad de Usuarios conectados: ");
+		lblUsuarios.setBounds(0, 464, 373, 14);
+		contentPane.add(lblUsuarios);
+		
+		String str[] = {"Pepe", "Juan", "Julio", "Lucas", "Leo"}; //TODO: Estos Usuarios deberian recibirse desde el servidor por el socket.
+		agregaUsuariosEnLista(str);
+		
+		VC.setVisible(true);
+	}
+	
+	private void abrirVentanaConfirmaSalir() {
+		int opcion = JOptionPane.showConfirmDialog(VC, "Desea salir del Chat", "Confirmación", JOptionPane.YES_NO_OPTION);
+		if(opcion == JOptionPane.YES_OPTION) {
+			if(conectado) {
+				try {
+					mySocket.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+					System.exit(0);
+				}
+			}
+			System.exit(0);
+		}
+	}
+	
+	public void agregaUsuariosEnLista(String str[]) {
+		DefaultListModel<String> modeloLista = new DefaultListModel<String>();
+		for(String item : str)
+			modeloLista.addElement(item);
+		listUsuarios.setModel(modeloLista);
+		lblUsuarios.setText("Cantidad de Usuarios Conectados: " + modeloLista.getSize());
+	}
+	
+	private void seleccionarElementoLista() {
+		if(!listUsuarios.isSelectionEmpty())
+			new VentanaChat(listUsuarios.getSelectedValue(), "Chat");
+		else
+			JOptionPane.showMessageDialog(VC, "Seleccione un elemento de la lista", "Seleccionar Usuario", JOptionPane.INFORMATION_MESSAGE);
+	}
+	
+	private void abrirVentanaConfiguracion() {
+		new VentanaConfiguracion(VC);
+	}
+	
+	private void seleccionaDobleClickChat(MouseEvent me) {
+		if(me.getClickCount() == 2)
+			new VentanaChat(listUsuarios.getSelectedValue(), "Chat");
+	}
 }
